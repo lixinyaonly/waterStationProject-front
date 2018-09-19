bbcc6562fe8a717a15981e9251dc873cc1c616a8
summer0101 <summer01_01@126.com>
2018-09-19 23:47:51 +0800

    导航demo

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,6 @@
 <template>
   <div id="app">
-    <el-menu :default-active="activeIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect">
-    <el-menu-item index="1">处理中心</el-menu-item>
-    <el-submenu index="2">
-      <template slot="title">我的工作台</template>
-      <el-menu-item index="2-1">选项1</el-menu-item>
-      <el-menu-item index="2-2">选项2</el-menu-item>
-      <el-menu-item index="2-3">选项3</el-menu-item>
-      <el-submenu index="2-4">
-        <template slot="title">选项4</template>
-        <el-menu-item index="2-4-1">选项1</el-menu-item>
-        <el-menu-item index="2-4-2">选项2</el-menu-item>
-        <el-menu-item index="2-4-3">选项3</el-menu-item>
-      </el-submenu>
-    </el-submenu>
-    <el-menu-item index="3" disabled>消息中心</el-menu-item>
-    <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-  </el-menu>
-  <div class="line"></div>
+	  <h1>鑫利康源</h1>
   <el-menu
     :default-active="activeIndex2"
     class="el-menu-demo"
@@ -48,29 +31,30 @@
 
 <script>
 export default {
-  name: 'App',
+  name: "App",
   data() {
-      return {
-        activeIndex: '1',
-        activeIndex2: '1'
-      };
-    },
+    return {
+      activeIndex: "1",
+      activeIndex2: "1"
+    };
+  },
   methods: {
-      handleSelect(key, keyPath) {
-        console.log(key, keyPath);
-      }
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
     }
-}
-
+  }
+};
 </script>
 
 <style>
+body{
+	margin: 0;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
